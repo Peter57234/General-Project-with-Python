2eaa0af98b9588a8bedd9c08f398a0fcc49b6b06
Nguyễn Đình Hưng <161201616+Peter57234@users.noreply.github.com>
2024-06-06 21:55:57 +0700

    Update general_project.py
    Signed-off-by: Nguyễn Đình Hưng <161201616+Peter57234@users.noreply.github.com>

--- general_project.py
+++ general_project.py
@@ -9,11 +9,78 @@ import pygame
 import qrcode
 import random
 import asyncio
+import secrets
+import string
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import bcrypt
+
 
 client = OpenAI(api_key=os.environ.get(""))
 
 x = PrettyTable()
 
+def login_and_register():
+  def hash_password(password):
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    # Hash the password
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
+  def register(): 
+    nickname = input("Nhập tên người dùng: ")
+    password = input("Nhập mật khẩu: ")
+    hashed_password = hash_password(password)
+          
+    with open('register_log.txt', 'w') as files:
+       files.write(nickname + "\n")
+       files.write(hashed_password.decode() + '\n')
+    print("Đăng ký thành công") 
+  def check_password(stored_password, provided_password):
+    # Check the password with the hashed version
+    return bcrypt.checkpw(provided_password.encode(), stored_password.encode())    
+       
+  def login():
+    max_attempts = 3
+    attempts = 0
+    while attempts < max_attempts:
+        nickname = input("Nhập tên người dùng: ")
+        password = input("Nhập mật khẩu: ")
+        
+        with open('register_log.txt', 'r') as file:
+            lines = file.readlines()
+            valid = False
+            
+            # Chạy qua các dòng, kiểm tra nickname và mật khẩu
+            for i in range(0, len(lines), 2):
+                saved_nickname = lines[i].strip()
+                saved_hashed_password = lines[i+1].strip()
+                
+                if nickname == saved_nickname and check_password(saved_hashed_password, password):
+                    valid = True
+                    break
+                 
+            
+        if valid:
+            print("Đăng nhập thành công!")
+            break
+        else:
+            attempts += 1
+            if attempts < max_attempts:
+               print(f"Sai mật khẩu hoặc tên người dùng, bạn còn {max_attempts - attempts} lần thử nữa.")
+            else:   
+             print("Sai mật khẩu hoặc tên người dùng, vui lòng thử lại sau vài phút.")
+             time.sleep(1000)
+  def main():
+     choice = input("Chọn 1 để đăng ký, 2 để đăng nhập: ")
+     if choice == '1':
+        register()
+        login()
+     if choice == '2':
+        login()
+  if __name__ == '__main__':
+     main()                
 
 def input_user(language):
     annouce = {
@@ -24,8 +91,8 @@ def input_user(language):
     return (input(annouce[language][i])for i in range(5))   
 def men(language):
    menus = {
-      'en' : ["1. Language", "2. Exit", "3. Table", "4. Convert decimals to binary", "5. Unit conversion", "6. Prime", "7. AI help", "8. Music", "9. Quick Response Code", "10. Probability ", "11. Caro game"],
-      'vi' : ["1. Ngôn ngữ", "2. Thoát", "3. Bảng", "4. Chuyển đổi số thập phân sang hệ nhị phân", "5. Chuyển đổi đơn vị đo", "6. Số nguyên tố", "7. AI trợ giúp", "8. Nhạc", "9. Mã phản hồi nhanh", "10. Xác suất ", "11. Chơi ca-rô"],
+      'en' : ["1. Language", "2. Exit", "3. Table", "4. Convert decimals to binary", "5. Unit conversion", "6. Prime", "7. AI help", "8. Music", "9. Quick Response Code", "10. Probability ", "11. Caro game", "12. Password Generator", "13. Banking", "14. Feedback", "15 Log out"],
+      'vi' : ["1. Ngôn ngữ", "2. Thoát", "3. Bảng", "4. Chuyển đổi số thập phân sang hệ nhị phân", "5. Chuyển đổi đơn vị đo", "6. Số nguyên tố", "7. AI trợ giúp", "8. Nhạc", "9. Mã phản hồi nhanh", "10. Xác suất ", "11. Chơi ca-rô", "12. Tạo mật khẩu", "13. Banking", "14. Nhận xét", "15. Đăng xuất"],
 
    } 
    for item in menus[language]:
@@ -84,6 +151,93 @@ def binary_convert():
     return print("Bằng: ", x)
   except Exception:
     print("Lỗi! Vui lòng nhập 1 con số hợp lệ") 
+
+
+def create_ticket():
+    # Tạo một mật khẩu ngẫu nhiên dài 8 ký tự
+    characters = string.ascii_letters + string.digits
+    ticket = ''.join(random.choice(characters) for i in range(8))
+    # Lưu mật khẩu vào file pwd.txt
+    with open("ticket.txt", 'w') as file:
+        file.write(ticket)
+    return ticket
+
+def send_feedback_via_email(feedback, feedback_email, recipient_email, stars):
+    # Cấu hình thông tin tài khoản Gmail của bạn
+    sender_email = "formyapplicationdonotreply@gmail.com" # Thay bằng email của bạn
+    sender_password = "nkmp dojx nnbj anox" # Thay bằng mật khẩu ứng dụng
+    
+
+    # Cấu hình email
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = f"Phản hồi từ người dùng {feedback_email}"
+    body = f"""Chào {recipient_email}! \n"""
+    body_2 = f"""Phản hồi của người dùng {feedback_email} là: {feedback}. \n"""
+    body_6 = f"""Đánh giá phần mềm: {stars} sao. \n"""
+    body_7 = """Hệ thống sẽ lưu lại phản hồi này dưới dạng email. \n"""
+    body_3 = """Bạn không được phép trả lời mail này. \n""" 
+    body_4 = """Xin cảm ơn, \n"""
+    body_5 = """The Administrator Team."""
+    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body_2, 'plain'))
+    msg.attach(MIMEText(body_6, 'plain'))
+    msg.attach(MIMEText(body_7, 'plain'))
+    msg.attach(MIMEText(body_3, 'plain'))
+    msg.attach(MIMEText(body_4, 'plain'))
+    msg.attach(MIMEText(body_5, 'plain'))
+    
+    try:
+        # Thiết lập kết nối và gửi email
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, recipient_email, text)
+        server.quit()
+        
+    except Exception as e:
+        print(f"Không thể gửi email: {e}") 
+
+def feedback():
+   while True: 
+    a = input("Nhận xét của người dùng: ")
+    e = input("Bạn đánh giá phần mềm này mấy sao trên thang điểm 1-5: ")
+    d = input("Bạn có muốn thoát không: ")
+    if 'yes' in d:
+       break 
+    
+    ticket_code = create_ticket()
+    print("------------------------------------------------------")
+    print("Mã vé của bạn là:  " + ticket_code)
+    print("Bạn có 15 phút để xác nhận rằng bạn không phải là bot.")
+    print("**Chú ý dấu cách, space.**")
+    print("Xin cảm ơn,")
+    print("The Administrator Team.")
+    print("------------------------------------------------------")
+    b = input("Mã vé: ")
+        
+    with open("ticket.txt", 'r') as file:
+        stored_code = file.read().strip()
+        if b != stored_code:
+            print("Sai!")
+            break
+        else:
+            print("Đúng!")
+    
+    c = input("Xác nhận?(Y/N): ")
+    if c.lower() == 'y':
+        feedback_email = input("Email của bạn là gì?: ")
+        recipient_email = "nguyendinhhung240112@gmail.com" 
+        send_feedback_via_email(a, feedback_email, recipient_email, e)
+    else:
+        print("Phản hồi không được gửi.")
+    print("Đang gửi phản hồi...")
+    time.sleep(10)
+    print("Phản hồi đã được gửi qua email thành công!")
+
+    
 def convert_unit():
   try:  
     ans = int(input("Nhập số mét: "))
@@ -226,76 +380,144 @@ def print_board(board):
     print()
 
 def check_win(board, player):
-    for row in range(3):
-       if all([cell == player for cell in board[row]]):
-          return True
-    for col in range(3):
-       if all([board[row][col] == player for row in range(3)]):
-          return True
-    if all([board[i][i] == player for i in range(3)]) or all([board[i][2-i] == player for i in range(3)]):
-       return True
+    for row in range(5):
+        if all([cell == player for cell in board[row]]):
+            return True
+    for col in range(5):
+        if all([board[row][col] == player for row in range(5)]):
+            return True
+    if all([board[i][i] == player for i in range(5)]) or all([board[i][4-i] == player for i in range(5)]):
+        return True
     return False
 
 def check_draw(board):
     return all([all([cell is not None for cell in row]) for row in board])
 
-def minimax(board, depth, alpha, beta, is_maximizing):
+def minimax(board, depth, alpha, beta, is_maximizing, max_depth):
     if check_win(board, 'O'):
         return 1
     if check_win(board, 'X'):
         return -1
     if check_draw(board):
         return 0
+    if depth >= max_depth:
+       return 0
 
     if is_maximizing:
         best_score = -math.inf
-        for row in range(3):
-            for col in range(3):
+        for row in range(5):
+            for col in range(5):
                 if board[row][col] is None:
                     board[row][col] = 'O'
-                    score = minimax(board, depth + 1, alpha, beta, False)
+                    score = minimax(board, depth + 1, alpha, beta, False, max_depth)
                     board[row][col] = None
                     best_score = max(score, best_score)
                     alpha = max(alpha, score)
                     if beta <= alpha:
-                       break
+                        break
         return best_score
     else:
         best_score = math.inf
-        for row in range(3):
-            for col in range(3):
+        for row in range(5):
+            for col in range(5):
                 if board[row][col] is None:
                     board[row][col] = 'X'
-                    score = minimax(board, depth + 1, alpha, beta, True)
+                    score = minimax(board, depth + 1, alpha, beta, True, max_depth)
                     board[row][col] = None
                     best_score = min(score, best_score)
                     beta = min(beta, score)
                     if beta <= alpha:
-                       break
+                        break
         return best_score
-    
 
-
-def best_move(board):
+def best_move(board, max_depth):
     best_score = -math.inf
     move = None
-    for row in range(3):
-        for col in range(3):
+    for row in range(5):
+        for col in range(5):
             if board[row][col] is None:
                 board[row][col] = 'O'
-                score = minimax(board, 0, -math.inf, math.inf, False)
+                score = minimax(board, 0, -math.inf, math.inf, False, max_depth)
                 board[row][col] = None
                 if score > best_score:
                     best_score = score
                     move = (row, col)
     return move
 
+
 # Khởi tạo bảng cờ ca rô 3x3
 board = [
-    [None, None, None],
-    [None, None, None],
-    [None, None, None]
+    [None, None, None, None, None],
+    [None, None, None, None, None],
+    [None, None, None, None, None],
+    [None, None, None, None, None],
+    [None, None, None, None, None],
 ]
+
+def banking():
+   def withdraw():
+      a = int(input("Nhập số tiền bạn muốn gửi vào tài khoản ngân hàng của bạn: "))
+      if a < 0:
+         print("Vui lòng nhập 1 con số hợp lệ")
+         return 0
+      else:
+         return a
+   def view(balance):
+     print(f"Số tiền của bạn là ${balance:.2f}")
+
+   def info():
+      pass   
+      
+   def deposit(balance):
+     a = int(input("Nhập số tiền bạn muốn rút ra: ")) 
+     if a > balance:
+        print("Vui lòng nhập 1 con số hợp lệ")
+        return 0
+     elif a < 0:
+        print("Vui lòng nhập 1 con số hợp lệ")
+        return 0 
+     else:
+        return a
+   def main(): 
+    balance = 0     
+    is_running = True
+    while is_running:
+     print("-------------------------")
+     print("Hệ thống ngân hàng")
+     print("1. Xem số dư tài khoản ngân hàng.")
+     print("2. Rút tiền")
+     print("3. Chuyển tiền vào tài khoản ngân hàng.")
+     print("4. Exit.")
+     ans = input("Chọn 1 lựa chọn: ")
+     if ans == '1':
+       view(balance)
+     elif ans == '2':
+       balance -= deposit(balance)
+     elif ans == '3':
+       balance += withdraw()
+     elif ans == '4':
+       print("Cảm ơn vì đã sử dụng hệ thống ngân hàng")   
+       is_running = False
+   if __name__ == '__main__':
+     main()          
+
+def create_pwd(pw_length=8):
+   letters = string.ascii_letters
+   digits = string.digits
+   special_chars = string.punctuation
+
+   alphabets = letters + digits + special_chars
+   pwd = ''
+   pwd_strong =  False
+
+   while not pwd_strong:
+      pwd += ''.join(secrets.choice(alphabets))
+      if (any(char in special_chars for char in pwd) and sum(char in digits for char in pwd) >= pw_length):
+        pwd_strong = True
+      with open("pwd.txt", 'w') as files:
+         files.write(pwd) 
+                           
+   return pwd
 
 def eratosthenes():
   while True:
@@ -410,6 +632,7 @@ def victory():
      
 async def main():
  try:
+  login_and_register()
   while True:
     print(f"started at {time.strftime('%X')}")
     language = 'en'
@@ -424,44 +647,53 @@ async def main():
       x.add_column("Column 2", )
       x.add_column("Column 3", )
       print(x)
+     elif '13' in opt:
+        banking() 
+     elif '12' in opt:
+        print(create_pwd()) 
+     elif '14' in opt:
+        feedback()
+     elif '15' in opt:
+        login_and_register()      
      elif '11' in opt:
-       print("Bắt đầu trò chơi cờ ca rô!")
-       print_board(board)
+      print("Bắt đầu trò chơi cờ ca rô!")
+      print_board(board)
 
-       while True:
-    # Người chơi X đi
-        row = int(input("Nhập hàng (0, 1, 2): "))
-        col = int(input("Nhập cột (0, 1, 2): "))
-        if board[row][col] is None:
-         board[row][col] = 'X'
-        else:
-          print("Ô đã được chọn. Vui lòng chọn ô khác.")
-          continue
+      while True:
+    # Player X move
+       row = int(input("Nhập hàng (0, 1, 2, 3, 4): "))
+       col = int(input("Nhập cột (0, 1, 2, 3, 4): "))
+       if board[row][col] is None:
+        board[row][col] = 'X'
+       else:
+        print("Ô đã được chọn. Vui lòng chọn ô khác.")
+        continue
     
-        print_board(board)
-        if check_win(board, 'X'):
-         print("Người chơi X thắng!")
-         break
-        if check_draw(board):
-         print("Hòa!")
-         break
+       print_board(board)
+       if check_win(board, 'X'):
+        print("Người chơi X thắng!")
+        break
+       if check_draw(board):
+        print("Hòa!")
+        break
 
-       # Bot O đi
-        print("Bot O đang suy nghĩ...")
-        move = best_move(board)
-        if move is not None:
-         board[move[0]][move[1]] = 'O'
-         print("Bot O đi: hàng {}, cột {}".format(move[0], move[1]))
-         print_board(board)
-         if check_win(board, 'O'):
+    # Bot O move
+       print("Bot O đang suy nghĩ...")
+       max_depth = 3
+       move = best_move(board, max_depth)
+       if move is not None:
+        board[move[0]][move[1]] = 'O'
+        print("Bot O đi: hàng {}, cột {}".format(move[0], move[1]))
+        print_board(board)
+        if check_win(board, 'O'):
             print("Bot O thắng!")
             break
-         if check_draw(board):
+        if check_draw(board):
             print("Hòa!")
             break
-        else:
-         print("Hòa!")
-         break 
+       else:
+        print("Hòa!")
+        break
      elif '10' in opt:
        print("1. Tung xúc xắc")
        print("2. Kéo búa bao")
